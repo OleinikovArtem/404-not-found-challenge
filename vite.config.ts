c7c6a5feed449e1f0ac36bd314b435fc755c4515
Artem Oleinikov <artem.oleinikov.web@gmail.com>
2023-08-26 09:41:10 +0300

    added gh-pages and updated README.md

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://oleinikovartem.github.io/',
+  base: '/404-not-found-challenge/',
   plugins: [react()]
 })
